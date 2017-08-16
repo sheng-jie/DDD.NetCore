f9ee566e34d11602b535e9cde798e7ac056cf489
shengjie_yan <ysjshengjie@live.cn>
2017-08-17 06:36:37 +0800

    1. 定义实体、值对象（使用层超类型）。2. 定义聚合根。

---
+++ DDD.NetCore/Entities/AggregateRoot.cs
@@ -0,0 +1,12 @@
+﻿namespace DDD.NetCore.Entities
+{
+    public class AggregateRoot : AggregateRoot<int>, IAggregateRoot
+    {
+
+    }
+
+    public class AggregateRoot<TPrimaryKey> : Entity<TPrimaryKey>, IAggregateRoot<TPrimaryKey>
+    {
+
+    }
+}
---
+++ DDD.NetCore/Entities/IAggregateRoot.cs
@@ -0,0 +1,12 @@
+﻿namespace DDD.NetCore.Entities
+{
+    public interface IAggregateRoot : IAggregateRoot<int>, IEntity
+    {
+
+    }
+
+    public interface IAggregateRoot<TPrimaryKey> : IEntity<TPrimaryKey>
+    {
+
+    }
+}
