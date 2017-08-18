01adf1554be4e629a9909c2699c48544f947c4df
shengjie_yan <ysjshengjie@live.cn>
2017-08-19 06:28:20 +0800

    添加IUserAppService

--- DDD.NetCore.Application/Users/IUserAppService.cs
+++ DDD.NetCore.Application/Users/IUserAppService.cs
@@ -1,4 +1,6 @@
-﻿using DDD.NetCore.Domain.Customers;
+﻿using DDD.NetCore.Domain.Authorization;
+using DDD.NetCore.Domain.Customers;
+using DDD.NetCore.Domain.Uow;
 
 namespace DDD.NetCore.Application.Users
 {
@@ -7,5 +9,25 @@ namespace DDD.NetCore.Application.Users
         void InitialCustomerForUser(string userId);
 
         Customer GetCustomerByUserId(string userId);
+    }
+
+    public class UserAppService : IUserAppService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UserAppService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public void InitialCustomerForUser(string userId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Customer GetCustomerByUserId(string userId)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
