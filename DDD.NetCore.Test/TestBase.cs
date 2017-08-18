1ce1c8d6c1ca6e444903e3a05e5a72350add844d
shengjie_yan <ysjshengjie@live.cn>
2017-08-19 07:17:15 +0800

    使用Sqlite内存数据库以支持关系型数据库事务等。

--- DDD.NetCore.Test/TestBase.cs
+++ DDD.NetCore.Test/TestBase.cs
@@ -1,17 +1,33 @@
 ﻿using System;
 using DDD.NetCore.Infrastructure.EfCore;
 using DDD.NetCore.Test.TestData;
+using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Options;
 
 namespace DDD.NetCore.Test
 {
     public class TestBase
     {
-        public ApplicationDbContext InMemoryTestDbContext { get; }
+        /// <summary>
+        /// 内存数据库，用于测试关系型数据库
+        /// </summary>
+        public ApplicationDbContext InMemoryTestDbContext { get; set; }
+
+        /// <summary>
+        /// 内存数据库，用于测试非关系型数据库
+        /// </summary>
+        public ApplicationDbContext InMemorySqliteTestDbContext { get; set; }
         
 
         public TestBase()
+        {
+            GetInMemoryDbContext();
+            GetSqliteInMemoryDbContext();
+        }
+
+        private ApplicationDbContext GetInMemoryDbContext()
         {
             var options = new DbContextOptionsBuilder<ApplicationDbContext>()
                 .UseInMemoryDatabase(Guid.NewGuid().ToString())
@@ -22,6 +38,26 @@ namespace DDD.NetCore.Test
             InMemoryTestDbContext = new ApplicationDbContext(options);
             GoodsTestData.Initialize(InMemoryTestDbContext);
             UsersTestData.CreateTestUser(InMemoryTestDbContext);
+            return InMemoryTestDbContext;
+        }
+
+        public ApplicationDbContext GetSqliteInMemoryDbContext()
+        {
+            // In-memory database only exists while the connection is open
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseSqlite(connection)
+                .Options;
+
+            InMemorySqliteTestDbContext = new ApplicationDbContext(options);
+            InMemorySqliteTestDbContext.Database.EnsureCreated();
+
+            GoodsTestData.Initialize(InMemorySqliteTestDbContext);
+            UsersTestData.CreateTestUser(InMemorySqliteTestDbContext);
+
+            return InMemorySqliteTestDbContext;
+
         }
     }
 }
--- DDD.NetCore.Test/UnitTest1.cs
+++ DDD.NetCore.Test/UnitTest1.cs
@@ -12,32 +12,68 @@ namespace DDD.NetCore.Test
         [Fact]
         public void Test1()
         {
-            var result = InMemoryTestDbContext.Database.EnsureCreated();
-            Assert.Equal(true, InMemoryTestDbContext.Goods.Any());
+            var result = InMemorySqliteTestDbContext.Database.EnsureCreated();
+            Assert.Equal(true, InMemorySqliteTestDbContext.Goods.Any());
 
         }
 
         [Fact]
         public void Test2()
         {
-            using (var trasnaction = InMemoryTestDbContext.Database.BeginTransaction())
+            var customerCount = InMemorySqliteTestDbContext.Customers.Count();
+            var shoppingCartCount  = InMemorySqliteTestDbContext.ShoppingCarts.Count();
+            using (var trasnaction = InMemorySqliteTestDbContext.Database.BeginTransaction())
             {
-                var user = InMemoryTestDbContext.Users.FirstOrDefault();
+                var user = InMemorySqliteTestDbContext.Users.FirstOrDefault();
                 var customer = new Customer() { ApplicationUserId = user.Id };
-                InMemoryTestDbContext.Customers.Add(customer);
-                InMemoryTestDbContext.SaveChanges();
+                InMemorySqliteTestDbContext.Customers.Add(customer);
+                InMemorySqliteTestDbContext.SaveChanges();
 
                 var cart = new ShoppingCart() { CustomerId = customer.Id };
-                InMemoryTestDbContext.ShoppingCarts.Add(cart);
-                InMemoryTestDbContext.SaveChanges();
+                InMemorySqliteTestDbContext.ShoppingCarts.Add(cart);
+                InMemorySqliteTestDbContext.SaveChanges();
 
                 customer.ShoppingCartId = cart.Id;
-                InMemoryTestDbContext.Entry(customer).State=EntityState.Modified;
-                InMemoryTestDbContext.SaveChanges();
+                InMemorySqliteTestDbContext.Entry(customer).State = EntityState.Modified;
+                InMemorySqliteTestDbContext.SaveChanges();
 
                 trasnaction.Commit();
             }
-            Assert.Equal(1,InMemoryTestDbContext.ShoppingCarts.Count());
+            Assert.Equal(customerCount + 1, InMemorySqliteTestDbContext.Customers.Count());
+            Assert.Equal(shoppingCartCount + 1, InMemorySqliteTestDbContext.ShoppingCarts.Count());
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var customerCount = InMemorySqliteTestDbContext.Customers.Count();
+            var shoppingCartCount = InMemorySqliteTestDbContext.ShoppingCarts.Count();
+            using (var trasnaction = InMemorySqliteTestDbContext.Database.BeginTransaction())
+            {
+                try
+                {
+                    var user = InMemorySqliteTestDbContext.Users.FirstOrDefault();
+                    var customer = new Customer() { ApplicationUserId = user.Id };
+                    InMemorySqliteTestDbContext.Customers.Add(customer);
+                    InMemorySqliteTestDbContext.SaveChanges();
+
+                    var cart = new ShoppingCart();//{ CustomerId = customer.Id };
+                    InMemorySqliteTestDbContext.ShoppingCarts.Add(cart);
+                    InMemorySqliteTestDbContext.SaveChanges();
+
+                    customer.ShoppingCartId = cart.Id;
+                    InMemorySqliteTestDbContext.Entry(customer).State = EntityState.Modified;
+                    InMemorySqliteTestDbContext.SaveChanges();
+
+                    trasnaction.Commit();
+                }
+                catch (System.Exception)
+                {
+                    trasnaction.Rollback();
+                }
+            }
+            Assert.Equal(customerCount, InMemorySqliteTestDbContext.Customers.Count());
+            Assert.Equal(shoppingCartCount, InMemorySqliteTestDbContext.ShoppingCarts.Count());
         }
     }
 }
